4685bf75738d5bdff88daf0600ac5bb65304ef75
nksm <nksm@ness.gr.jp>
2016-07-03 14:46:45 +0900

    stateコンポーネントなどを追加

--- react-dom-example/src/main.js
+++ react-dom-example/src/main.js
@@ -8,6 +8,73 @@ class HelloComponent extends React.Component {
   }
 }
 
+class PropsComponent extends React.Component {
+  render() {
+    const { target } = this.props;
+    return (
+      <div>Hello {target}!</div>
+    );
+  }
+}
+
+class StateComponent extends React.Component {
+  constructor() {
+    super();
+    //初期状態
+    this.state = {
+      target: 'CodeGrid'
+    };
+  }
+  render() {
+    let { target } = this.state;
+    return (
+      <div>Hello {target}!</div>
+    );
+  }
+}
+
+class TextInputComponent extends React.Component {
+  constructor() {
+    super();
+
+    // 入力された内容を保持しておく state
+    this.state = {
+      val: 'CodeGrid'
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+
+  // input 要素の change イベントの度に呼ばれるハンドラ
+  handleInputChange(ev) {
+    this.setState({
+      val: ev.target.value
+    });
+  }
+
+  // button 要素の click イベントに対応
+  handleButtonClick() {
+    this.setState({
+      val: ''
+    });
+  }
+
+  render() {
+    let { val } = this.state;
+
+    return(
+      <div>
+        <input type="text" onChange={this.handleInputChange} value={val} />
+        <button type="button" onClick={this.handleButtonClick}>Clear</button>
+        <hr />
+        {val}
+      </div>
+    );
+  }
+}
+
+
 // 表示するコンポーネント2
 class AppComponent extends React.Component {
   render() {
@@ -15,6 +82,8 @@ class AppComponent extends React.Component {
       <section>
         <h1>Hello App!</h1>
         <HelloComponent></HelloComponent>
+        <PropsComponent target="CodeGrid"></PropsComponent>
+        <StateComponent></StateComponent>
       </section>
     );
   }
